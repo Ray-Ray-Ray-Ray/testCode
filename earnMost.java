b0af8c7116af49c673f22f461875c9063febd57d
ray <liu6434@uwlax.edu>
2021-07-15 10:40:39 +0800

    second

---
+++ earnMost.java
@@ -0,0 +1,42 @@
+
+public class earnMost {
+   public static void main(String[] args) {
+       int[] stock = new int[]{4, 7, 4, 3, 5,9,};
+       System.out.println(earnMoney(stock));
+       System.out.println(lowestPriceToEarn(stock));
+   }
+
+   public static int earnMoney(int[] prices) {
+       int mostFee = 0;
+       int currentFee = 0;
+       for (int i = 0; i < prices.length; i++) {
+           for (int j = i + 1; j < prices.length; j++) {
+               if (prices[i] - prices[j] <= 0) {
+                   currentFee = Math.abs(prices[i] - prices[j]);
+               }
+               if (currentFee > mostFee) {
+                   mostFee = currentFee;
+               }
+           }
+       }
+       return mostFee;
+
+   }
+
+   public static int lowestPriceToEarn(int[] prices) {
+       int minPrice = Integer.MAX_VALUE;
+       int maxFee = 0;
+       int current = 0;
+       for(int i = 0; i<prices.length; i++){
+           if(prices[i] < minPrice){
+               minPrice = prices[i];
+           }else if(prices[i] > minPrice){  
+               current = prices[i] - minPrice; 
+               if(current > maxFee){
+                   maxFee = current;
+               }
+           }
+       }
+       return maxFee;
+   }
+}
